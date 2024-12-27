2a1caf2894a7f72faad39e92c0799aafdcbde023
JessicaFonsou <jessicas.fonsecas@gmail.com>
2024-12-27 09:23:40 -0300

    Primeiro commit

---
+++ src/main/java/com/kaspper/orcamentoSoftware/OrcamentoSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.kaspper.orcamentoSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaspper/orcamentoSoftware/OrcamentoSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaspper.orcamentoSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
